e6a2688b5e1997da9abadbeb90febd85f5e6145d
paltacky907 <66708330+paltacky907@users.noreply.github.com>
2022-04-17 01:57:04 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import Navbar from './components/layout/Navbar.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<Navbar />
+    </div>
+  );
+}
+
+export default App;
